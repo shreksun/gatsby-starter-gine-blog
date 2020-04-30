ee238602d904d3670321b18656e4f904e5bd5ced
sunzhongqi <48233882+shreksun@users.noreply.github.com>
2020-04-30 11:46:37 +0800

    fix config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,13 +3,13 @@ module.exports = {
         {
             resolve: `gatsby-theme-gine-blog`,
             options: {
-                configTable: "https://www.notion.so/shreksun/cf21d60264dd4a949a4d2d9d935af670?v=0c9620e43bec481b9933b58d9db51fef"
+                configTable: "https://www.notion.so/cf21d60264dd4a949a4d2d9d935af670?v=0c9620e43bec481b9933b58d9db51fef"
             }
         },
         {
             resolve: `gatsby-source-notion-database`,
             options: {
-                configTable: "https://www.notion.so/shreksun/f28296dc29644855a75a1b42f0296f49?v=2bf262683b50473db5904349a2d13270"
+                configTable: "https://www.notion.so/f28296dc29644855a75a1b42f0296f49?v=2bf262683b50473db5904349a2d13270"
             }
         }
     ],
